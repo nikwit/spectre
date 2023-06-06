be80f6b4c385e8cff1a4ba4351adf8f284646623
Nikolas <niko.wittek@gmail.com>
2023-06-06 10:41:24 +0200

    use geodesic acceleration

--- src/Evolution/Systems/CurvedScalarWave/Worldtube/SingletonActions/UpdateFunctionsOfTime.hpp
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/SingletonActions/UpdateFunctionsOfTime.hpp
@@ -9,11 +9,17 @@
 #include "ControlSystem/UpdateFunctionOfTime.hpp"
 #include "DataStructures/DataBox/DataBox.hpp"
 #include "DataStructures/DataBox/Prefixes.hpp"
+#include "Evolution/Systems/CurvedScalarWave/BackgroundSpacetime.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Worldtube/Inboxes.hpp"
 #include "Evolution/Systems/CurvedScalarWave/Worldtube/Tags.hpp"
 #include "Parallel/AlgorithmExecution.hpp"
 #include "Parallel/GlobalCache.hpp"
 #include "ParallelAlgorithms/EventsAndTriggers/Tags.hpp"
+#include "PointwiseFunctions/AnalyticSolutions/GeneralRelativity/KerrSchild.hpp"
+#include "PointwiseFunctions/GeneralRelativity/Christoffel.hpp"
+#include "PointwiseFunctions/GeneralRelativity/DerivativesOfSpacetimeMetric.hpp"
+#include "PointwiseFunctions/GeneralRelativity/InverseSpacetimeMetric.hpp"
+#include "PointwiseFunctions/GeneralRelativity/SpacetimeMetric.hpp"
 #include "Time/Actions/ChangeSlabSize.hpp"
 #include "Time/Tags.hpp"
 #include "Time/TimeStepId.hpp"
@@ -33,6 +39,8 @@ namespace CurvedScalarWave::Worldtube::Actions {
  */
 struct UpdateFunctionsOfTime {
   static constexpr size_t Dim = 3;
+  // using compute_tags =
+  // tmpl::list<Tags::InertialParticlePositionCompute<Dim>>;
   using inbox_tags = tmpl::list<>;
 
   template <typename DbTagsList, typename... InboxTags, typename Metavariables,
@@ -48,6 +56,8 @@ struct UpdateFunctionsOfTime {
     const auto& time_step = db::get<::Tags::TimeStep>(box);
     const auto& functions_of_time =
         Parallel::get<::domain::Tags::FunctionsOfTime>(cache);
+    const auto& excision_sphere = db::get<Tags::ExcisionSphere<Dim>>(box);
+    const auto& maps = Parallel::get<Tags::WorldtubeCoordinateMaps>(cache);
     const std::string rot_function_of_time_name = "Rotation";
     const std::string expansion_fot_name = "Expansion";
     const std::string size_a_fot_name = "SizeA";
@@ -58,22 +68,98 @@ struct UpdateFunctionsOfTime {
     const double current_fot_expiration_time =
         rot_function_of_time->time_bounds()[1];
     if (time > current_fot_expiration_time) {
-      double z_angular_acc = 0.;
-      if (time > 500.) {
-        z_angular_acc = 0.00006;
-      }
-      if (time > 1500.) {
-        z_angular_acc = -0.00006;
-      }
-      DataVector new_angular_acc(3, 0.);
-      new_angular_acc.at(2) = z_angular_acc;
-      const double envelope_radius = 50.;
+      const auto& inertial_particle_position =
+          maps(excision_sphere.center(), db::get<Tags::PreviousTime>(box),
+               functions_of_time);
+      const auto& particle_velocity =
+          std::get<3>(maps.coords_frame_velocity_jacobians(
+              excision_sphere.center(), db::get<Tags::PreviousTime>(box),
+              functions_of_time));
+      const auto& kerr_schild =
+          db::get<CurvedScalarWave::Tags::BackgroundSpacetime<
+              gr::Solutions::KerrSchild>>(box);
+      const auto spacetime_vars = kerr_schild.variables(
+          inertial_particle_position, time,
+          tmpl::list<
+              gr::Tags::Lapse<double>,
+              gr::Tags::Shift<double, Dim, Frame::Inertial>,
+              gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>,
+              gr::Tags::InverseSpatialMetric<double, Dim, Frame::Inertial>,
+              ::Tags::dt<gr::Tags::Lapse<double>>,
+              ::Tags::deriv<gr::Tags::Lapse<double>, tmpl::size_t<Dim>,
+                            Frame::Inertial>,
+              ::Tags::dt<gr::Tags::Shift<double, Dim, Frame::Inertial>>,
+              ::Tags::deriv<gr::Tags::Shift<double, Dim, Frame::Inertial>,
+                            tmpl::size_t<Dim>, Frame::Inertial>,
+              ::Tags::dt<gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>>,
+              ::Tags::deriv<
+                  gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>,
+                  tmpl::size_t<Dim>, Frame::Inertial>>{});
+      const auto inverse_spacetime_metric = gr::inverse_spacetime_metric(
+          get<gr::Tags::Lapse<double>>(spacetime_vars),
+          get<gr::Tags::Shift<double, Dim, Frame::Inertial>>(spacetime_vars),
+          get<gr::Tags::InverseSpatialMetric<double, Dim, Frame::Inertial>>(
+              spacetime_vars));
+      const auto d_spacetime_metric = gr::derivatives_of_spacetime_metric(
+          get<gr::Tags::Lapse<double>>(spacetime_vars),
+          get<::Tags::dt<gr::Tags::Lapse<double>>>(spacetime_vars),
+          get<::Tags::deriv<gr::Tags::Lapse<double>, tmpl::size_t<Dim>,
+                            Frame::Inertial>>(spacetime_vars),
+          get<gr::Tags::Shift<double, Dim, Frame::Inertial>>(spacetime_vars),
+          get<::Tags::dt<gr::Tags::Shift<double, Dim, Frame::Inertial>>>(
+              spacetime_vars),
+          get<::Tags::deriv<gr::Tags::Shift<double, Dim, Frame::Inertial>,
+                            tmpl::size_t<Dim>, Frame::Inertial>>(
+              spacetime_vars),
+          get<gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>>(
+              spacetime_vars),
+          get<::Tags::dt<
+              gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>>>(
+              spacetime_vars),
+          get<::Tags::deriv<
+              gr::Tags::SpatialMetric<double, Dim, Frame::Inertial>,
+              tmpl::size_t<Dim>, Frame::Inertial>>(spacetime_vars));
 
-      const double period = 100.;
-      const double omega = 2. * M_PI / period;
-      const double amp = 0.25;
-      const double expansion_acc = amp * omega * omega * cos(omega * time) *
-                                   sqrt(4. * M_PI) * envelope_radius;
+      const auto christoffel = gr::christoffel_second_kind(
+          d_spacetime_metric, inverse_spacetime_metric);
+
+      tnsr::I<double, Dim> particle_acceleration{};
+      for (size_t i = 0; i < Dim; ++i) {
+        particle_acceleration.get(i) =
+            particle_velocity.get(i) * christoffel.get(0, 0, 0) -
+            christoffel.get(i + 1, 0, 0);
+        for (size_t j = 0; j < Dim; ++j) {
+          particle_acceleration.get(i) +=
+              2. * particle_velocity.get(j) *
+              (particle_velocity.get(i) * christoffel.get(0, j + 1, 0) -
+               christoffel.get(i + 1, j + 1, 0));
+          for (size_t k = 0; k < Dim; ++k) {
+            particle_acceleration.get(i) +=
+                particle_velocity.get(j) * particle_velocity.get(k) *
+                (particle_velocity.get(i) * christoffel.get(0, j + 1, k + 1) -
+                 christoffel.get(i + 1, j + 1, k + 1));
+          }
+        }
+      }
+      const double& x = get<0>(inertial_particle_position);
+      const double& y = get<1>(inertial_particle_position);
+      const double& xdot = get<0>(particle_velocity);
+      const double& ydot = get<1>(particle_velocity);
+      const double& xddot = get<0>(particle_acceleration);
+      const double& yddot = get<1>(particle_acceleration);
+      const double r = hypot(x, y);
+      const double radial_acc =
+          (xddot * x + yddot * y + xdot * xdot + ydot * ydot) / r -
+          square(xdot * x + ydot * y) / cube(r);
+      const double angular_acc = (x * yddot - y * xddot) / square(r) -
+                                 2. * (x * xdot + y * ydot) *
+                                     (x * ydot - y * xdot) / square(square(r));
+      DataVector new_angular_acc(3, 0.);
+      new_angular_acc.at(2) = angular_acc;
+
+      const double envelope_radius = 50.;
+      const double expansion_acc =
+          -10. * radial_acc * sqrt(4. * M_PI) * envelope_radius;
 
       const auto& expansion_fot = functions_of_time.at(expansion_fot_name);
       const double object_radius = 1.;
