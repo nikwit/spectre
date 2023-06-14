c1d91e9b43176b900c79c2c3a86791ad75efb8e3
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2023-06-14 09:56:16 +0200

    Change constraint gammas

--- src/Evolution/Systems/CurvedScalarWave/Worldtube/ElementActions/InitializeConstraintGammas.cpp
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/ElementActions/InitializeConstraintGammas.cpp
@@ -20,9 +20,9 @@ void InitializeConstraintDampingGammas<Dim>::apply(
     const tnsr::I<DataVector, Dim, Frame::Inertial>& inertial_coords) {
   const size_t number_of_grid_points = get<0>(inertial_coords).size();
   *gamma1 = Scalar<DataVector>{number_of_grid_points, 0.};
-  const double amplitude = 10.;
-  const double sigma = 1.e-1;
-  const double constant = 1.e-4;
+  const double amplitude = 8.;
+  const double sigma = 2.e-1;
+  const double constant = 1.e-3;
   const auto radius = magnitude(inertial_coords);
   get(*gamma2) = amplitude * exp(-square(sigma * radius.get())) + constant;
 }
