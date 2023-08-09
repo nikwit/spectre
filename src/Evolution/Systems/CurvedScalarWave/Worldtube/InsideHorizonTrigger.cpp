99c24e01b5661a0e0349db61001ed63edb10e511
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2023-08-09 15:06:58 +0200

    add inside horizon trigger

---
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/InsideHorizonTrigger.cpp
@@ -0,0 +1,8 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Evolution/Systems/CurvedScalarWave/Worldtube/InsideHorizonTrigger.hpp"
+
+namespace Triggers {
+PUP::able::PUP_ID InsideHorizon::my_PUP_ID = 0;  // NOLINT
+}  // namespace Triggers
---
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/InsideHorizonTrigger.hpp
@@ -0,0 +1,57 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <limits>
+#include <pup.h>
+
+#include "DataStructures/Tensor/EagerMath/Magnitude.hpp"
+#include "Domain/Structure/ExcisionSphere.hpp"
+#include "Evolution/Systems/CurvedScalarWave/Worldtube/Tags.hpp"
+#include "ParallelAlgorithms/EventsAndTriggers/Trigger.hpp"
+#include "Time/TimeStepId.hpp"
+#include "Utilities/Serialization/CharmPupable.hpp"
+#include "Utilities/TMPL.hpp"
+
+/// \cond
+namespace Tags {
+struct TimeStepId;
+}  // namespace Tags
+/// \endcond
+
+namespace Triggers {
+/// \ingroup EventsAndTriggersGroup
+/// \ingroup TimeGroup
+/// Trigger based on a comparison with the slab number.
+
+class InsideHorizon : public Trigger {
+ public:
+  /// \cond
+  InsideHorizon() = default;
+  explicit InsideHorizon(CkMigrateMessage* /*unused*/) {}
+  using PUP::able::register_constructor;
+  WRAPPED_PUPable_decl_template(InsideHorizon);  // NOLINT
+  /// \endcond
+
+  static constexpr Options::String help =
+      "Trigger in intervals surrounding particular times.";
+  static constexpr size_t Dim = 3;
+  using options = tmpl::list<>;
+  using argument_tags = tmpl::list<
+      CurvedScalarWave::Worldtube::Tags::ParticlePositionVelocity<Dim>,
+      CurvedScalarWave::Worldtube::Tags::ExcisionSphere<Dim>>;
+
+  bool operator()(const std::array<tnsr::I<double, Dim, Frame::Inertial>, 2>&
+                      position_and_velocity,
+                  const ExcisionSphere<Dim>& excision_sphere) const {
+    const double orbit_radius = get(magnitude(position_and_velocity[0]));
+    const double wt_radius = excision_sphere.radius() * orbit_radius /
+                             get(magnitude(excision_sphere.center()));
+    return orbit_radius + wt_radius < 1.99 ? true : false;
+  }
+
+  // NOLINTNEXTLINE(google-runtime-references)
+  void pup(PUP::er& p) override {}
+};
+}  // namespace Triggers
