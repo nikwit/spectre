4e465a42174c6d7dadcdf78c098548b26e957f28
Nikolas Wittek <nikolas.wittek@aei.mpg.de>
2023-10-21 12:56:49 +0200

    center constraint gaussian around worldtube

--- src/Evolution/Systems/CurvedScalarWave/Worldtube/ElementActions/InitializeConstraintGammas.cpp
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/ElementActions/InitializeConstraintGammas.cpp
@@ -20,10 +20,13 @@ void InitializeConstraintDampingGammas<Dim>::apply(
     const tnsr::I<DataVector, Dim, Frame::Inertial>& inertial_coords) {
   const size_t number_of_grid_points = get<0>(inertial_coords).size();
   *gamma1 = Scalar<DataVector>{number_of_grid_points, 0.};
-  const double amplitude = 12.;
+  const double amplitude = 8.;
   const double sigma = 1.e-1;
   const double constant = 1.e-3;
-  const auto radius = magnitude(inertial_coords);
+
+  auto inertial_coords_centered = inertial_coords;
+  inertial_coords_centered.get(0) -= 7.;
+  const auto radius = magnitude(inertial_coords_centered);
   get(*gamma2) = amplitude * exp(-square(sigma * radius.get())) + constant;
 }
 
