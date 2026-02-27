21ab73357ba9fa931c35f3e030ce5e7488ab0da0
nikwit <nwittek@caltech.edu>
2026-02-27 10:31:43 -0800

    cmake: treat yaml-cpp headers as system includes

--- cmake/SetupYamlCpp.cmake
+++ cmake/SetupYamlCpp.cmake
@@ -26,6 +26,20 @@ if (NOT TARGET yaml-cpp::yaml-cpp)
   target_link_libraries(yaml-cpp::yaml-cpp INTERFACE yaml-cpp)
 endif()
 
+# Silence warnings originating from yaml-cpp headers in dependent targets.
+set(_yamlcpp_target yaml-cpp::yaml-cpp)
+get_target_property(_yamlcpp_aliased_target ${_yamlcpp_target} ALIASED_TARGET)
+if (_yamlcpp_aliased_target)
+  set(_yamlcpp_target ${_yamlcpp_aliased_target})
+endif()
+get_target_property(_yamlcpp_include_dirs
+  ${_yamlcpp_target} INTERFACE_INCLUDE_DIRECTORIES)
+if (_yamlcpp_include_dirs
+    AND NOT _yamlcpp_include_dirs STREQUAL "_yamlcpp_include_dirs-NOTFOUND")
+  set_property(TARGET ${_yamlcpp_target} APPEND PROPERTY
+    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_yamlcpp_include_dirs}")
+endif()
+
 set_property(
   GLOBAL APPEND PROPERTY SPECTRE_THIRD_PARTY_LIBS
   yaml-cpp::yaml-cpp
